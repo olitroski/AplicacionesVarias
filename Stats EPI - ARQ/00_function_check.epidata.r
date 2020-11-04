67c2cba2ffb81722d09ee4eaa269238eea8336d7
Oliver Rojas <oliver.rojas.b@gmail.com>
2020-11-04 01:23:50 -0300

    Update 00_function_check.epidata.r

--- Stats EPI - ARQ/00_function_check.epidata.r
+++ Stats EPI - ARQ/00_function_check.epidata.r
@@ -8,40 +8,46 @@
 check.epidata <- function(data = NULL){
     # Muestra un ejemplo, no requiere datos
 	if (class(data) == "NULL"){
-		cat("Los datos tienen que tener esta forma:\n")
-		cat("---------------------------------------------------------------------------------------\n")	
-		cat("     id            fec.hora estado dur_min mean_act dia.noc seq.dia   hora dia hora.abs\n")
-		cat("  10013 2016-08-03 07:35:00      W      85    273.4     Dia      01  7.583 Wed        7\n")
-		cat("  10013 2016-08-03 09:00:00      S     180     10.8     Dia      01  9.000 Wed        9\n")
-		cat("  10013 2016-08-03 12:00:00      W      17    136.7     Dia      01 12.000 Wed       12\n")
-		cat("  10013 2016-08-03 12:17:00      S      20      0.1     Dia      01 12.283 Wed       12\n")
-		cat("  10013 2016-08-03 12:37:00      W     213    247.4     Dia      01 12.617 Wed       12\n")
-		cat("  10013 2016-08-03 16:10:00      S      21     30.1     Dia      01 16.167 Wed       16\n")
-		cat("---------------------------------------------------------------------------------------\n\n")	
+	    cat("\n")
+	    cat("+----------------------------------------------------------------------------------------+\n")	
+		cat("| Los datos tienen que tener esta forma:                                                 |\n")
+		cat("+----------------------------------------------------------------------------------------+\n")	
+		cat("|     id            fec.hora estado dur_min mean_act dia.noc seq.dia   hora dia hora.abs |\n")
+		cat("|  10013 2016-08-03 07:35:00      W      85    273.4     Dia      01  7.583 Wed        7 |\n")
+		cat("|  10013 2016-08-03 09:00:00      S     180     10.8     Dia      01  9.000 Wed        9 |\n")
+		cat("|  10013 2016-08-03 12:00:00      W      17    136.7     Dia      01 12.000 Wed       12 |\n")
+		cat("|  10013 2016-08-03 12:17:00      S      20      0.1     Dia      01 12.283 Wed       12 |\n")
+		cat("|  10013 2016-08-03 12:37:00      W     213    247.4     Dia      01 12.617 Wed       12 |\n")
+		cat("|  10013 2016-08-03 16:10:00      S      21     30.1     Dia      01 16.167 Wed       16 |\n")
+		cat("+----------------------------------------------------------------------------------------+\n\n")	
 		
 		# Podrian haber 2 formatos
-		if (class(data[,1]) == "character"){
-		    Formato = c("character", "POSIXct POSIXt", "character", "numeric", 
-		                "numeric", "character","character", "numeric", "character", "numeric")
-		} else if (class(data[,1]) == "numeric") {
-		    Formato = c("numeric", "POSIXct POSIXt", "character", "numeric", 
-		                "numeric", "character","character", "numeric", "character", "numeric")
-		}
-		
 		formatok <- data.frame(
-		    Variables = c("id", "fec.hora", "estado", "dur_min", "mean_act", "dia.noc", "seq.dia", "hora", 
-		                  "dia", "hora.abs"),
-		    Formato = Formato
-		)			
-		print(formatok)
+		    Variables = c("id", "fec.hora", "estado", "dur_min", "mean_act", "dia.noc", "seq.dia", "hora", "dia", "hora.abs"),
+		    Formato = c("character", "POSIXct POSIXt", "character", "numeric", "numeric", "character","character", "numeric", "character", "numeric"),
+		    stringsAsFactors = FALSE
+		)
+		formatok$Formato[1] <- "character o numeric"
+		
+		cat("\n")
+		cat("+----------------------------------------------------------------------------------------+\n")	
+		cat("| Las variables deben tener los siguientes formatos:                                     |\n")
+		cat("+----------------------------------------------------------------------------------------+\n")	
+    	print(formatok)
+    	cat("+----------------------------------------------------------------------------------------+\n")	
+		cat("\n")
+        
 		
 	} else {
-		# Chequear si estan las varaibles necesarias y seleccionar
+	    cat("\n")
+	    cat("<<<----------Revisando archivo de datos----------->>>\n")
+
+		# Chequear nombre de variables
 		checkvar <- c("id", "fec.hora", "estado", "dur_min", "mean_act", "dia.noc", "seq.dia", "hora", "dia", "hora.abs")
 		varmatch <- sum(names(data) %in% checkvar)
 
 		if (length(checkvar) == varmatch){
-			cat("\nVarNames Ok - ")
+			cat("      Chequeando nombres de las variables... Ok\n")
 		} else {
 			cat("Nombres de variables no provienen de un epi.data, se necesitan estas:\n")
 			cat(checkvar, sep = " - "); cat("\n")
@@ -72,7 +78,7 @@ check.epidata <- function(data = NULL){
             stop("Revisar datos  :)")
             
         } else if (sum(format == formatok) == 10){
-			cat("VarClass Ok - ")
+			cat("      Chequeando formato de las variables... Ok\n")
 			
 		} else {
 			cat("Class de las variables no hace match con un epi.data, se necesitan estas:\n")
@@ -80,6 +86,6 @@ check.epidata <- function(data = NULL){
 			stop()
 		}
 		
-		cat("Archivo de EPI ok \n")
+		cat("<<<------Archivo de episodios (EPI) correcto------>>>\n")
 	}
 }
